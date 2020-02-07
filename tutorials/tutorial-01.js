70dba290481dd5f244b0450f30c27497a4ab599e
Ginkgoch <ginkgoch@outlook.com>
2020-02-07 16:13:57 +0800

    331: Add taiwan map.

--- tutorials/tutorial-01.js
+++ tutorials/tutorial-01.js
@@ -14,28 +14,42 @@ async function main() {
     let provinceLayer = createLayerWithDefaultStyle('../data/chn/gadm36_CHN_1_3857.shp');
     connectDynamicData(provinceLayer);
 
+    let taiwanLayer = createLayerWithDefaultStyle('../data/chn/TWN_adm1_3857.shp');
+    taiwanLayer.source.dynamicFields.push({ name: 'confirmedCount', fieldsDependOn: [], mapper: f => 15 });
+    taiwanLayer.source.dynamicFields.push({ name: 'NL_NAME_2', fieldsDependOn: [], mapper: f => '台湾' });
+
     // add fill color based confirmed count level
     let confirmedCountStyle = _getClassBreakStyle('confirmedCount');
     provinceLayer.styles.push(confirmedCountStyle);
+    taiwanLayer.styles.push(confirmedCountStyle);
+
+    let getLabelStyle = fieldName => {
+        let labelStyle = new GK.TextStyle(fieldName, 'black', 'Arial 12px');
+        labelStyle.lineWidth = 2;
+        labelStyle.strokeStyle = 'white';
+        labelStyle.location = 'interior';
+        return labelStyle;
+    };
 
     // add province label
-    let provinceLabelStyle = new GK.TextStyle('[NL_NAME_1]', 'black', 'Arial 12px');
-    provinceLabelStyle.lineWidth = 2;
-    provinceLabelStyle.strokeStyle = 'white';
-    provinceLabelStyle.location = 'interior';
+    let provinceLabelStyle = getLabelStyle('[NL_NAME_1]');
     provinceLayer.styles.push(provinceLabelStyle);
+
+    let taiwanLabelStyle = getLabelStyle('[NL_NAME_2]');
+    taiwanLayer.styles.push(taiwanLabelStyle);
 
     let mapEngine = new GK.MapEngine(imageWidth, imageHeight);
     mapEngine.srs = new GK.Srs('EPSG:900913');
     mapEngine.pushLayer(worldLayer);
     mapEngine.pushLayer(provinceLayer);
+    mapEngine.pushLayer(taiwanLayer);
 
     await provinceLayer.open();
     let chinaEnvelope = await provinceLayer.envelope();
     chinaEnvelope = GK.ViewportUtils.adjustEnvelopeToMatchScreenSize(chinaEnvelope, imageWidth, imageHeight);
 
     let image = await mapEngine.image(chinaEnvelope);
-    fs.writeFileSync(path.resolve(__dirname, './images/tutorial-01-final.png'), image.toBuffer());
+    fs.writeFileSync(path.resolve(__dirname, './images/tutorial-01-china-confirmed.png'), image.toBuffer());
 }
 
 function createLayerWithDefaultStyle(filePath) {
@@ -90,8 +104,8 @@ function _getClassBreakStyle(field) {
     const strokeWidth = 1;
 
     let countStops = [1, 10, 50, 100, 300, 500, 750, 1000, Number.MAX_SAFE_INTEGER];
-    // let activePallette = ['#fff5f0', '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a', '#ef3b2c', '#cb181d', '#67000d'];
-    let activePallette = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c'];
+    let activePallette = ['#fff5f0', '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a', '#ef3b2c', '#cb181d', '#67000d'];
+    // let activePallette = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c'];
     let style = new GK.ClassBreakStyle(field);
 
     for (let i = 1; i < countStops.length; i++) {
